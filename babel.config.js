b26cf9142599c9d74dd4ea9e36f5c4558a3b46e6
luiz.souza <lgsouza.sp@gmail.com>
2023-05-13 15:50:02 -0300

    change assets to src - configure babel module resolver

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,20 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      ['module-resolver', {
+        root: ['./src'],
+        alias: {
+          '@assets': './assets',
+          '@components': './components',
+          '@routes': './routes',
+          '@screens': './screens',
+          '@storage': './storage',
+          '@theme': './theme',
+          '@types': './types',
+          '@utils': './utils'
+        }
+      }]
+    ]
   };
 };
